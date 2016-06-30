cd2fad4be165e4d32ca2b49573a4068ceaef9fdf
Gorav Chhina <Gorav Chhina>
2016-06-29 20:20:57 -0700

    second commit

---
+++ Day02/app.js
@@ -0,0 +1,9 @@
+var fname = prompt("What is your first name?");
+var lname = prompt("What is your last name");
+var fullname = fname + lname;
+
+console.log(fname);
+confirm(lname);
+var resp = confirm("yes or no");
+alert(resp);
+document.write(fullname);
--- Day02/style.js
+++ Day02/style.js
@@ -1,1 +1,12 @@
-alert("Here is alert2!");
+/*alert("Here is alert2!");
+
+var userResp = confirm("What is your name");
+var name = prompt("what is your name");
+
+console.log(name);*/
+
+var fname = prompt("What is your first name?");
+var lname = prompt("What is your last name");
+
+console.log(fname);
+confirm(lname);
