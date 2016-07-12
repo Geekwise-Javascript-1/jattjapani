2533fa8dfebbd1fe68260e88a452623c6181f2a5
Gorav Chhina <Gorav Chhina>
2016-07-11 23:58:52 -0700

    Day04

--- Day03/app.js
+++ Day03/app.js
@@ -27,13 +27,13 @@ console.log(c);
 console.log(typeof c);
 
 console.log(isNaN(''));
-*/
+
 var age = prompt("What is your age?")
 var gender = prompt("What is your gender?")
 console.log(age);
 console.log(gender);
 
-//age = parseInt(age);
+age = parseInt(age);
 
 
 if (gender == "Male" || gender == "M" || gender == "male" || gender == "m") {
@@ -62,3 +62,67 @@ if (gender == "Male" || gender == "M" || gender == "male" || gender == "m") {
  else  {
     alert("You are " + age + " years old but we don't know your gender?")
 }
+*/
+/*
+var age = prompt('What is your age?');
+var gender = prompt('What is your gender')
+
+ switch (age) {
+   case age < 18 && gender == "male":
+     alert("You are " + age + " year old Male and below 18")
+     break;
+  case age >= 18 && age <=50 && gender == "male":
+    alert(" You are " + age + "year old Male and between 18-50")
+    break;
+   default:
+    console.log('nothing left');
+ }
+ */
+/*
+  var age = prompt ('Type youe age');
+  age = parseInt(age);
+
+  var result = age < 18 ? alert('young') : alert ('old');
+
+  console.log(result);
+*/
+// ******************Day 03 Assignment **********************
+
+var option; //variable for user option entry
+var number; // varaible for storing number entry
+// Ask user to Enter  option 1 or 2
+option = prompt("Please Select an Opton\n" + "1 - Enter a number.\n" +
+    " 2 - End Program");
+
+// Check if user Entry is valid either 1 or 2
+if (!isNaN(option) && option != "" && option != "2") {
+      alert("You Entered option 1.")
+      console.log("Option " + typeof(option));
+
+
+    number = prompt("Enter a number."); //Ask user to enter a number
+    console.log("Number " + typeof(number));
+    //Check if user entry was a number then store result as true or false
+    var isNumber = isNaN(number);
+    console.log("isNumber " + typeof(isNumber) + " " + isNumber);
+    // Use switch statements to alert user of their entry
+    switch (isNumber) {
+        // If not a number or true then alert user
+        case true && number != "":
+            alert("This is not a number!")
+            break;
+        // If user enrty is a number or false then alert user
+        case false:
+            alert(number + " Is  a number. Good Job!")
+            break;
+        default:
+            alert("The End")
+    }
+
+// Alert user of invalid option entry
+} else  if (option == "2"){
+       alert("The End!")
+}
+  else {
+    alert("Invalid Entry! \n Try Again")
+  }
---
+++ Day04/scripts.js
@@ -0,0 +1,86 @@
+/*var doSomething = prompt("What would you like to do")
+var newString;
+console.log(doSomething);
+console.log(doSomething.length);
+console.log(doSomething.indexOf("d"));
+console.log(doSomething.charAt(0));
+
+for (i = 0, i < doSomething.length, i++){
+  if { (doSomething.charAt(i) == " "){
+
+  }
+  else
+    newString.charAt(i) = doSomething.charAt(i);
+
+}
+}
+*/
+/*
+var number1 = parseInt(prompt("Please enter a number"));
+
+if (isNaN(number1)) {
+  alert("This is not a number!")
+  }
+  else {
+    var number2 = parseInt(prompt("Please enter a 2nd number"));
+    if (isNaN(number2)){
+      alert("This is not a number!")
+    }
+  }
+   if ( !isNaN(number1) && !isNaN(number2)) {
+
+     var multiply = parseInt(number1) * parseInt(number2);
+     var divide   = parseInt(number1) / parseInt(number2);
+     console.log(multiply);
+     console.log(divide);
+   }
+   else {
+       alert("Your Entries were incorrect!")
+   }
+   */
+/*
+   var myString = "red , green , yellow, blue";
+   var string3 = "";
+
+   var string1 = myString.indexOf("y");
+   console.log(string1);
+
+   var string2 = myString.indexOf("w");
+   console.log(string2);
+
+   for (i = string1; i <= string2; i++){
+     string3 = string3 + myString.charAt(i);
+   }
+
+   console.log(string3);
+  */
+
+/*  var randNum = Math.random();
+  var multiply = randNum * 11;
+  var round = Math.floor(multiply);
+
+  console.log(randNum);
+  console.log(multiply);
+  console.log(round);
+  console.log(Math.ceil(multiply));
+  console.log(Math.round(multiply));
+
+  var date = new Date();
+  var daysOfWeek = ['sun','mon', 'tue', 'wed', 'thur', 'fri', 'sat'];
+
+  console.log(daysOfWeek[date.getDay()]);
+*/
+
+function upperCase(string) {
+  var firstLetter = string.charAt(0);
+  var firstLetterUppercase = firstLetter.toUpperCase()
+  var sliceString = string.slice(1);
+
+  var newUpperCaseString = firstLetterUppercase + sliceString;
+
+  console.log(newUpperCaseString);
+}
+
+var name = prompt("Enter your name");
+
+upperCase(name);
